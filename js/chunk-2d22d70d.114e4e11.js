b9b5aae7bcae886d8f3b86913760050222520d50
Ashley Winter <ashleywntr@gmail.com>
2022-06-26 15:55:04 +0100

    Web ready

--- js/chunk-2d22d70d.114e4e11.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d70d"],{f805:function(e,n,o){"use strict";o.r(n);var t=function(){var e=this,n=e.$createElement,o=e._self._c||n;return o("b-container",[o("b-row",[o("b-col",[o("h1",[o("b",[e._v("Page not Found")])]),e._v(" The supplied url could not be found. Please proceed to "),o("a",{attrs:{href:"./"}},[e._v("home")])])],1)],1)},u=[],r={name:"404Route"},a=r,c=o("2877"),l=Object(c["a"])(a,t,u,!1,null,"580e13fb",null);n["default"]=l.exports}}]);
-//# sourceMappingURL=chunk-2d22d70d.114e4e11.js.map
